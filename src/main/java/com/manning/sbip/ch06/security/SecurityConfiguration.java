f174f302aaf4f35ea3ead576d3e88f79a8355b71
olamide <myyde8170@gmail.com>
2022-11-16 21:09:56 +0100

    adding BCryptPasswordEncoder to SecurityConfiguration

--- src/main/java/com/manning/sbip/ch06/security/SecurityConfiguration.java
+++ src/main/java/com/manning/sbip/ch06/security/SecurityConfiguration.java
@@ -1,9 +1,12 @@
 package com.manning.sbip.ch06.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -20,5 +23,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers("/webjars/**", "/images/*", "/css/*", "/h2-console/**");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
